266608b358b9e0919db1529568567e8cb325409f
nssk <nssk1999@gmail.com>
2021-05-10 16:51:35 +0530

    update

--- predictor/Metadata.py
+++ predictor/Metadata.py
@@ -1,57 +1,12 @@
 def getmetadata(filename):
-    import librosa
+    from python_speech_features import mfcc
+    import scipy.io.wavfile as wav
     import numpy as np
 
+    (rate, sig) = wav.read(filename)
+    mfcc_feat = mfcc(sig, rate, winlen=0.020, nfft=960, appendEnergy=False)
+    covariance = np.cov(np.matrix.transpose(mfcc_feat))
+    mean_matrix = mfcc_feat.mean(0)
+    feature = (mean_matrix, covariance, 0)
 
-    y, sr = librosa.load(filename)
-    #fetching tempo
-
-    onset_env = librosa.onset.onset_strength(y, sr)
-    tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
-
-    #fetching beats
-
-    y_harmonic, y_percussive = librosa.effects.hpss(y)
-    tempo, beat_frames = librosa.beat.beat_track(y=y_percussive,sr=sr)
-
-    #chroma_stft
-
-    chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-
-    #rmse
-
-    rmse = librosa.feature.rms(y=y)
-
-    #fetching spectral centroid
-
-    spec_centroid = librosa.feature.spectral_centroid(y, sr=sr)[0]
-
-    #spectral bandwidth
-
-    spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-
-    #fetching spectral rolloff
-
-    spec_rolloff = librosa.feature.spectral_rolloff(y+0.01, sr=sr)[0]
-
-    #zero crossing rate
-
-    zero_crossing = librosa.feature.zero_crossing_rate(y)
-
-    #mfcc
-
-    mfcc = librosa.feature.mfcc(y=y, sr=sr)
-
-    #metadata dictionary
-
-    metadata_dict = {'tempo':tempo,'chroma_stft':np.mean(chroma_stft),'rmse':np.mean(rmse),
-                     'spectral_centroid':np.mean(spec_centroid),'spectral_bandwidth':np.mean(spec_bw), 
-                     'rolloff':np.mean(spec_rolloff), 'zero_crossing_rates':np.mean(zero_crossing)}
-
-    for i in range(1,21):
-        metadata_dict.update({'mfcc'+str(i):np.mean(mfcc[i-1])})
-
-    return list(metadata_dict.values())
-
-
-
+    return feature
--- predictor/predict.py
+++ predictor/predict.py
@@ -1,15 +1,59 @@
+import numpy as np
+import operator
+
+
+def distance(instance1, instance2, k):
+    distance = 0
+    mm1 = instance1[0]
+    cm1 = instance1[1]
+    mm2 = instance2[0]
+    cm2 = instance2[1]
+    distance = np.trace(np.dot(np.linalg.inv(cm2), cm1))
+    distance += (np.dot(np.dot((mm2-mm1).transpose(),
+                               np.linalg.inv(cm2)), mm2-mm1))
+    distance += np.log(np.linalg.det(cm2)) - np.log(np.linalg.det(cm1))
+    distance -= k
+    return distance
+
+
+def getNeighbors(trainingSet, instance, k):
+    distances = []
+    for x in range(len(trainingSet)):
+        dist = distance(trainingSet[x], instance, k) + \
+            distance(instance, trainingSet[x], k)
+        distances.append((trainingSet[x][2], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+
+def nearestClass(neighbors):
+    classVote = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x]
+        if response in classVote:
+            classVote[response] += 1
+        else:
+            classVote[response] = 1
+    sorter = sorted(classVote.items(),
+                    key=operator.itemgetter(1), reverse=True)
+    return sorter[0][0]
+
 
 def predict_gen(meta1):
     import pickle
     import os
     from django.conf import settings
-    path = os.path.join(settings.MODELS, 'models.p')
-    with open(path, 'rb') as pickled:
-        data = pickle.load(pickled)
-    svmp = data['svmp']
-    norma = data['norma']
-    lgn = data['lgn']
-    x = norma.transform([meta1])
-    pred = svmp.predict(x)
-    return(lgn[pred[0]])
-
+    dataset = []
+    path = os.path.join(settings.MODELS, 'data.dat')
+    with open(path, 'rb') as f:
+        while True:
+            try:
+                dataset.append(pickle.load(f))
+            except EOFError:
+                f.close()
+                break
+    pred = nearestClass(getNeighbors(dataset, meta1, 5))
+    return(pred)
