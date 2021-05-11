c03571b13719c1e7b8b5acd55d7dcfe031d7b716
N S S KOUSHIIK <40133414+nssk1999@users.noreply.github.com>
2021-05-11 08:30:30 +0530

    debug settings

--- MusicClassification/settings.py
+++ MusicClassification/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c2!j!65n$n8lbsp0vqk=(9#v9+irenmrvie(lqw5*)k&l!ku4#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
