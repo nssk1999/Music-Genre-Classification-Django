f87ee4c67241b41b47dd0fa1e90275d1fcbebf79
nssk <nssk1999@gmail.com>
2021-05-12 13:12:29 +0530

    trial2fix static files 2

--- MusicClassification/urls.py
+++ MusicClassification/urls.py
@@ -25,7 +25,9 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    #test mode
+    # test mode
     from django.conf.urls.static import static
-    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
